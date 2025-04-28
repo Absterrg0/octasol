00df6099cfb962dda912de1f82cedbe27daa8507
ritikbhatt20 <ritikbhatt020@gmail.com>
2025-04-28 13:53:55 +0000

    fix(build): static to dynamic authHeader feature

--- src/app/api/usersubmissions/route.ts
+++ src/app/api/usersubmissions/route.ts
@@ -8,6 +8,8 @@ import {
 import { logToDiscord } from "@/utils/logger";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   try {
     const authHeader = req.headers.get("Authorization");
